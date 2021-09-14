4c587d6596c152b105af170c08d43fd3f5ac6677
Pavel Kryuchkov <p.kryuchkov@appline.ru>
2021-09-14 21:16:51 +0300

    2  и 3 задание

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -1,16 +1,17 @@
 /**
  * @author Pavel Kryuchkov
- * @see #main
  * @param x - Первое число
  * @param y - Второе число
  * @param z - Знак арифметического действия
  * @return Результат арифметического действия
+ * @see #main
  */
 
 //Реализовать сложение дробных чисел, которые пользователь вводит в консоль.
 // Использовать комментарии и JavaDoc при описании метода.
 // Использовать форматирование при выводе результата в консоль.
 // Полученный результат округлять до 4-х знаков после запятой.
+
 import java.util.Scanner;
 
 public class Base {
@@ -26,18 +27,31 @@ public class Base {
         String z = oin.nextLine();
         switch (z) {
             case "+":
-                System.out.printf("x + y = %.4f", (x + y));
+                System.out.printf("x + y = %.4f%n", (x + y));
                 break;
             case "-":
-                System.out.printf("x - y = %.4f", (x - y));
+                System.out.printf("x - y = %.4f%n", (x - y));
                 break;
             case "*":
-                System.out.printf("x * y = %.4f", (x * y));
+                System.out.printf("x * y = %.4f%n", (x * y));
                 break;
             case "/":
-                System.out.printf("x / y = %.4f", (x / y));
+                if (y == 0) {
+                    System.out.println("На ноль делить нельзя");
+                } else {
+                    System.out.printf("x / y = %.4f%n" + (x / y));
+                }
                 break;
         }
-
+        Scanner arraysizein = new Scanner(System.in);
+        System.out.println("Введите размер массива: ");
+        int arraysize = arraysizein.nextInt();
+        String[] array = new String[arraysize];
+        for (int i = 0; i < arraysize; i++ ) {
+            Scanner textin = new Scanner(System.in);
+            System.out.println("Введите текст");
+            String text = textin.nextLine();
+            array[i] = text;
+        }
     }
 }
